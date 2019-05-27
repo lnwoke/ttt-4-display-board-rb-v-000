48852db197cfa42eae28f2dc1c6ec3378f420a5c
Leslie Nwoke <drleslie@heartworknow.com>
2019-05-27 17:44:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ def display_board(board)
     puts "-----------"
     puts "   | #{board[4]} |   "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]}  | #{board[8]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = ["X", "X", "X", " ", "X", " ", "O", "O", "O"]
